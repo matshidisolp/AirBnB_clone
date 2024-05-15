d18f64c647aba887627c7e207e3fd4e97a6f6328
Terminal <terminalkarabo@gmail.com>
2024-05-15 00:54:31 -0700

    a class BaseModel that defines all common attributes/methods for other classes:

--- models/base_model.py
+++ models/base_model.py
@@ -22,16 +22,27 @@ class BaseModel:
                 create_at: datetime of creation
                 updated_at: datetime of last update
         """
-    if kwargs:
-        for key, value in kwargs.items():
-            if key == 'created_at' or key == 'updated_at':
-                setattr(self, key, datetime.strptime(
-                 value, "%Y-%m-%dT%H:%M:%S.%f"))
-            elif key != '__class__':
-                setattr(self, key, value)
-    else:
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(
+                    value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
         self.updated_at = datetime.now()
 
-
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
